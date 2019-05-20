64377907a5026f74818b32216df3e9708515c78c
JuanGongora <gongora.animations@gmail.com>
2019-05-20 19:48:51 -0400

    setting up the routes of app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
+import { routes } from "./routes.js";
 
 Vue.use(VueRouter);
 
+const router = new VueRouter({
+  // https://router.vuejs.org/guide/essentials/history-mode.html#html5-history-mode
+  mode: 'history',
+  // es6 shortcut to routes: routes
+  routes
+});
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 });
