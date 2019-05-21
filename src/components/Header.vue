011a2170d4539937dff7e4f1a3a2ff9f38b73a8c
JuanGongora <gongora.animations@gmail.com>
2019-05-21 18:53:01 -0400

    fixed typo in bootstrap html

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
                 <router-link to="/" class="navbar-brand">Stock Trader</router-link>
             </div>
             <div class="collapse navbar-collapse">
-                <ul class="nav navar-nav">
+                <ul class="nav navbar-nav">
                     <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
                     <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
                 </ul>
