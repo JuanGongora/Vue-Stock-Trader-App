027aa7c66cac86cc365180dc4860e854bbf890d5
JuanGongora <gongora.animations@gmail.com>
2019-05-20 22:30:42 -0400

    updated stock component view and data

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,27 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <div>
+        <h1>The Stocks Component</h1>
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
 
 <script>
+    import Stock from './Stock.vue'
     export default {
-        name: "Stocks"
+        name: "Stocks",
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: 'BMW', price: 110},
+                    {id: 2, name: 'Google', price: 200},
+                    {id: 3, name: 'Apple', price: 250},
+                    {id: 3, name: 'Twitter', price: 20}
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
     }
 </script>
 
